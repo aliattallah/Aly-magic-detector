9358d68464799f1b44d0fdce09abb37f3b9bb38d
aliattallah <alyhesham,alyh@gmail.com>
2020-07-05 03:25:43 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,26 +1,160 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import Navigation from './components/Navigation/Navigation';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Logo from './components/Logo/Logo'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const app = new Clarifai.App({
+ apiKey: 'ba5596e938644533a15ac340f5448cbb'
+});
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 70,
+      density: {
+        enable: true,
+        value_area: 800 
+      },
+      shape:  {
+        type: 'star'
+      },
+      size: {
+        value: 20,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 10
+        }
+
+      },
+      line_linked: {
+        enable: true
+      }
+    }
+
+ }
+ }
+
+const intialState = {
+      input: '',
+      imageUrl: '' ,
+      box: {},
+      route: 'signin',
+      isSignedIn: false,
+      user: {
+    id: '',
+    name: '',
+    email: '',
+    password: '',
+    entries: 0,
+    joined: ''
+      }
+    }
+class App extends Component {
+  constructor() {
+    super();
+    this.state = intialState
+  }
+ // cors npm dah mohem nekk 3lsh hwar origin accecss bt3 http eli fash5ne f weather app abl kda
+loadUser =(data) => {
+  this.setState({user: {
+     id: data.id,
+    name: data.name,
+    email: data.email,
+    password: data.password,
+    entries: data.entries,
+    joined: data.joined
+  }})
+} 
+
+calculateFaceLocation = (data) => {
+  const clarifaiFace= data.outputs[0].data.regions[0].region_info.bounding_box;
+  const image = document.getElementById('inputimage');;
+  const width= Number(image.width);
+  const height= Number(image.height)
+  console.log(width, height);
+  return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+
+
+}
+displayFaceBox= (box) => {
+  console.log(box)
+  this.setState ({box: box});
+  }
+
+onInputChange= (event) => {
+  this.setState ({input: event.target.value});
+}
+
+onButtonSubmit= () => {
+ this.setState ({imageUrl: this.state.input})
+  app.models.predict(
+    'c0c0ac362b03416da06ab3fa36fb58e3', 
+    this.state.input)
+  .then(response =>  {
+    if(response) {
+      fetch('https://beautiful-yosemite-31335.herokuapp.com/image', {
+        method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+       id: this.state.user.id
+      })
+      })
+      .then(response => response.json())
+      .then(count => {
+        this.setState(Object.assign(this.state.user, {entries: count}))
+      })
+    }
+    this.displayFaceBox(this.calculateFaceLocation(response))})
+  .catch(err => console.log(err))
+}
+onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState(intialState)
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
+
+  render() {
+    return (
+      <div className="App">
+       <Particles className='particles'
+              params={particlesOptions}
+            />
+      <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
+      { this.state.route=== 'home'
+      ? <div>
+      <Logo />
+      <Rank name={this.state.user.name} entries={this.state.user.entries} />
+      <ImageLinkForm 
+      onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+      <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+      </div>
+      : (
+        this.state.route === 'signin'
+         ?  <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/> 
+         :  <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+         )
+      
+    }
+      </div>
+      );
+  }
 }
 
 export default App;
